57cc2168282ca080406d374ea40b5d196c618739
beerent <brentryczak@gmail.com>
2021-09-11 08:05:08 -0500

    cleaned code, added walk up animation

--- Merlin.gd
+++ Merlin.gd
@@ -9,23 +9,72 @@ var velocity = Vector2.ZERO
 onready var merlin = $AnimatedSprite
 
 func _physics_process(delta):
+	var input = get_movement_input(delta)
+	handle_movement(input, delta)
+
+
+func get_movement_input(delta):
 	var input = Vector2.ZERO
 	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input = input.normalized()
+
+	return input
+
+func handle_movement(input, delta):
 	
 	if input != Vector2.ZERO:
-		if input.x == 1:
-			merlin.play("walk")
-			merlin.flip_h = false
-		elif input.x == -1:
-			merlin.play("walk")
-			merlin.flip_h = true
-		
-		
-		velocity = velocity.move_toward(input * MAX_SPEED, ACCELERATION * delta)
+		handle_walk_direction(input)
+		handle_positive_walk_velocity(input, delta)
 	else:
-		merlin.play("stand")
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		idle()
+		handle_negative_walk_velocity(input, delta)
 
 	move_and_collide(velocity * delta)
+
+func handle_walk_direction(input):
+	if is_walking_right(input):
+		walk_right()
+	elif is_walking_left(input):
+		walk_left()
+	
+	if is_walking_up(input):
+		walk_up()
+	elif is_walking_down(input):
+		walk_down()
+
+func handle_positive_walk_velocity(input, delta):
+	velocity = velocity.move_toward(input * MAX_SPEED, ACCELERATION * delta)
+
+func handle_negative_walk_velocity(input, delta):
+	velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+func is_walking_right(input):
+	return input.x == 1
+
+func is_walking_left(input):
+	return input.x == -1
+
+func is_walking_up(input):
+	return input.y == -1
+
+func is_walking_down(input):
+	return input.y == 1
+
+func walk_right():
+	merlin.play("walk")
+	merlin.flip_h = false
+
+func walk_left():
+	merlin.play("walk")
+	merlin.flip_h = true
+
+func walk_up():
+	merlin.play("vert_walk_up")
+	merlin.flip_v = false
+
+func walk_down():
+	merlin.play("vert_walk")
+	merlin.flip_v = false
+func idle():
+	merlin.play("stand")
